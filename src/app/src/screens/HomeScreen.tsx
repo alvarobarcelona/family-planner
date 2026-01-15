9b8a4df5be29f728778008aa2b0ab9d7a91735d4
alvarobarcelona <alvaro90barcelona@gmail.com>
2026-01-15 22:34:41 +0100

    text changed

--- src/app/src/screens/HomeScreen.tsx
+++ src/app/src/screens/HomeScreen.tsx
@@ -128,7 +128,7 @@ export function HomeScreen() {
             Cerrar Sesión
           </button>
         </div>
-        
+
 
 
 
@@ -206,8 +206,13 @@ export function HomeScreen() {
       )}
 
 
-      
-      <div className="text-gray-500 text-xl"> # Esto es lo que tiene la familia para hoy #</div>
+
+      <div className="flex flex-col items-center justify-center gap-1 mt-4 mb-2">
+        <div className="text-gray-500 text-lg font-medium text-center"> # Esto es lo que tiene la familia para hoy #</div>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-gray-400 animate-bounce">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+        </svg>
+      </div>
 
       <div>Filtros</div>
       {/* Filtros por miembro */}
